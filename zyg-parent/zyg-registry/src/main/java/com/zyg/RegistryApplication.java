feb029569a0527298db1195e7ae82554f37d14c5
zhangyuangang <a88615255>
2021-03-10 10:55:16 +0800

    提交代码

---
+++ zyg-parent/zyg-registry/src/main/java/com/zyg/RegistryApplication.java
@@ -0,0 +1,20 @@
+package com.zyg;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IDEA
+ * @author :zyg
+ * @date :2021/01/27
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@EnableApolloConfig
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
