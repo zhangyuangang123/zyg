386c830fbc70561ec7bf29c3c761ce89340c14b8
zhangyuangang <a88615255>
2021-03-12 09:35:53 +0800

    提交代码

---
+++ zyg-parent/zyg-getway/script/start.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#打包镜像启动服务
+#author：SHIYULONG
+docker_repostory=192.168.16.61/zyg/
+server_name=zyg-getway
+server_tag=:1.0.0-SNAPSHOT
+#服务路径
+workdir=$(dirname  "$(pwd)")
+#工程路径
+topdir=${workdir%zyg-parent*}
+#core包路径
+coredir="zyg-parent/zyg-core"
+echo "workspace dir:" $workdir
+echo "core dir:"$topdir$coredir
+active=$1
+if [ $active"x" == "devx" ]; then
+	echo "打包环境：开发环境"
+elif [ $active"x" == "fatx" ]; then
+	echo "打包环境：测试环境"
+elif [ $active"x" == "prodx" ]; then
+	echo "打包环境：生产环境"
+else
+	echo "未知的打包环境"
+	exit 1
+fi
+echo "----git pull 开始----"
+#git pull >> /dev/null
+#if (( $? ))
+#then
+#	echo "git pull failed"
+#	exit 1
+#else
+#	echo "git pull success"
+#fi
+echo "----zyg-core install----"
+
+cd "$topdir$coredir"
+#git pull >> /dev/null
+mvn clean install >> /dev/null
+if (( $? ))
+then
+	echo "zyg-core：mvn install failed"
+	exit 1
+else
+	echo "zyg-core：mvn install success"
+fi
+#cd "$topdir$commondir"
+#echo "----caption-basics-server install----"
+#git pull >> /dev/null
+#mvn clean install >> /dev/null
+#if (( $? ))
+#then
+#	echo "caption-basics-server：mvn install failed"
+#	exit 1
+#else
+#	echo "caption-basics-server：mvn install success"
+#fi
+cd $workdir
+echo "mvn package：${workdir##*/}"
+mvn clean package -P$active >> /dev/null
+if (( $? ))
+then
+	echo "mvn package failed"
+	exit 1
+else
+       	echo "mvn package success"
+fi
+#镜像名
+image_name="$server_name-$1$server_tag"
+if [ $active = "dev" ] || [ $active = "fat" ];
+then
+	#关闭容器、删除容器
+	container_id=`docker ps -q --filter name=$server_name`
+	if [ ! -z $container_id ];
+	then
+		docker stop $container_id
+		echo "stop container"
+		docker rm $container_id
+		echo "rm container"
+	fi
+	echo "start running docker images：$image_name"
+	docker run --name "$server_name" -d --restart=always -v "/data/zyg/logs/$server_name/":/log  --network=host "$docker_repostory$image_name"
+	#启动日志
+	docker logs -f $(docker ps -q --filter name=$server_name)|while read line
+	do
+        	is_startup=`echo $line|grep "Completed initialization"|wc -l`
+		is_failed=`echo $line|grep "APPLICATION FAILED TO START"|wc -l`
+        	if [ $is_startup -eq 1 ] ; then
+                	echo "docker images run complete:$image_name"
+			exit 1
+        	fi
+		if [ $is_failed -eq 1 ] ; then
+			echo "docker images run failed:$image_name"
+			exit 1
+		fi
+	done
+fi
+if [ $active = "dev" ] || [ $active = "fat" ];
+then
+	exit 1
+fi
+if [ $active == "prod" ];
+then
+docker push "$docker_repostory$image_name"
+	echo -n "Enter your IP address:"
+	read ip_address
+	echo "pull images"
+	ssh root@$ip_address docker pull "$docker_repostory$image_name"
+	echo "stop container:$server_name"
+	ssh root@$ip_address docker stop $(docker ps -q -a --filter name=$server_name)
+	echo "images run"
+	ssh root@$ip_address docker run --name $server_name"-`date +%m%d`" --restart=always -d -v /data/zyg/logs/zyg-getway/"`date +%m%d`":/log  --network=host "$docker_repostory$image_name"
+	echo "docker images run complete:$image_name"
+	exit 1
+fi
--- zyg-parent/zyg-getway/src/main/java/com/zyg/getway/GetwayApplication.java
+++ zyg-parent/zyg-getway/src/main/java/com/zyg/getway/GetwayApplication.java
@@ -1,5 +1,20 @@
 package com.zyg.getway;
 
-public class GetwayApplication {
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Created with IDEA
+ * @author :zyg
+ * @date :2021/01/27
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@EnableApolloConfig
+public class GetwayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayApplication.class,args);
+    }
 }
