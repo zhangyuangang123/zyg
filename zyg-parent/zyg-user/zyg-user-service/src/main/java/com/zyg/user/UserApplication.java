9265caf1c9929b6f3bc892d26e8fe7e78ee4a1a7
zhangyuangang <a88615255>
2021-03-17 11:34:47 +0800

    提交代码

--- zyg-parent/zyg-user/zyg-user-service/src/main/java/com/zyg/user/UserApplication.java
+++ zyg-parent/zyg-user/zyg-user-service/src/main/java/com/zyg/user/UserApplication.java
@@ -4,14 +4,21 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Created with IDEA
  * @author :zyg
  * @date :2021/03/15
  */
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.zyg.user.*","com.zyg.core.*"})
+@EnableTransactionManagement
+@EnableEurekaClient
+@EnableFeignClients
+@MapperScan(value = "com.zyg.*.mapper")
 @EnableApolloConfig
 public class UserApplication {
     public static void main(String[] args) {
