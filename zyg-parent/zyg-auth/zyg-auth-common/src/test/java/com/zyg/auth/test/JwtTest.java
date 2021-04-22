09b0a38b50a16242f4f774e6a90c0a2134700dd1
zhangyuangang <a88615255>
2021-04-22 09:03:41 +0800

    提交代码

---
+++ zyg-parent/zyg-auth/zyg-auth-common/src/test/java/com/zyg/auth/test/JwtTest.java
@@ -0,0 +1,49 @@
+package com.zyg.auth.test;
+
+import com.zyg.auth.entity.UserInfo;
+import com.zyg.auth.utils.JwtUtils;
+import com.zyg.auth.utils.RsaUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class JwtTest {
+
+    private static final String pubKeyPath = "e:\\rsa\\rsa.pub";
+
+    private static final String priKeyPath = "e:\\rsa\\rsa.pri";
+
+    private PublicKey publicKey;
+
+    private PrivateKey privateKey;
+
+    @Test
+    public void testRsa() throws Exception {
+        RsaUtils.generateKey(pubKeyPath, priKeyPath, "234");
+    }
+
+    //@Before
+    public void testGetRsa() throws Exception {
+        this.publicKey = RsaUtils.getPublicKey(pubKeyPath);
+        this.privateKey = RsaUtils.getPrivateKey(priKeyPath);
+    }
+
+    @Test
+    public void testGenerateToken() throws Exception {
+        // 生成token
+        String token = JwtUtils.generateToken(new UserInfo(20L, "jack"), privateKey, 5);
+        System.out.println("token = " + token);
+    }
+
+    @Test
+    public void testParseToken() throws Exception {
+        String token = "eyJhbGciOiJSUzI1NiJ9.eyJpZCI6MjAsInVzZXJuYW1lIjoiamFjayIsImV4cCI6MTYxNjU0NzcxMX0.BLKaiMRFVkLTyOfPb1iynRjoFc4vbIpZvg1BLDqClgoIcLQmMJQ7JKU99HFUc5bl5mzqX6kF3wj0fghftTb2WhGlajcLmeFVdOfjVqN3wKS0cwBe0la6AgeoPeL_8ozp_i27t7rlajaTte-SDYQ9o0kru_Gbl_oDssS-9lKT5S8";
+
+        // 解析token
+        UserInfo user = JwtUtils.getInfoFromToken(token, publicKey);
+        System.out.println("id: " + user.getId());
+        System.out.println("userName: " + user.getUsername());
+    }
+}
--- zyg-parent/zyg-getway/src/main/java/com/zyg/getway/filter/GwCorsFilter.java
+++ zyg-parent/zyg-getway/src/main/java/com/zyg/getway/filter/GwCorsFilter.java
@@ -3,8 +3,8 @@ package com.zyg.getway.filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.util.pattern.PathPatternParser;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * @program: zyg-parent
@@ -15,24 +15,20 @@ import org.springframework.web.util.pattern.PathPatternParser;
 @Configuration
 public class GwCorsFilter {
     @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 允许cookies跨域
-        config.addAllowedOrigin(CorsConfiguration.ALL);// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
-        config.addAllowedHeader(CorsConfiguration.ALL);// #允许访问的头信息,*表示全部
-        config.setMaxAge(18000L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
-        config.addAllowedMethod("OPTIONS");// 允许提交请求的方法类型，*表示全部允许
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
+    public CorsFilter corsWebFilter() {
+        //初始化 cors 配置对象
+        CorsConfiguration configuration=new CorsConfiguration();
 
-        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source =
-                new org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource(new PathPatternParser());
-        source.registerCorsConfiguration("/**", config);
+        //允许跨域的域名，如果要携带cookie，不能写*。* 代表所有域名都可以跨域访问
+        configuration.addAllowedOrigin("http://192.168.6.128");
+        configuration.setAllowCredentials(true); //允许携带cookie
+        configuration.addAllowedMethod("*"); //代表所有的请求方法 Get Post Put Delete
+        configuration.addAllowedHeader("*"); //允许携带任何头信息
 
-        return new CorsWebFilter(source);
+        //初始化 cors 配置源对象
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource=new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",configuration);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
